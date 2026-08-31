fc9377d5f1be1abb8197e89fa1cd88103c3b44a3
ankitsharma2006 <ankitth126@gmail.com>
2026-08-31 21:55:51 +0530

    from_dict def

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import json
 from datetime import datetime
 
-last_id=0
+with open("task.json", "r") as f:
+   data=json.load(f)
+
+last_id = 0
 
 def generate_id():
     global last_id
@@ -25,8 +28,13 @@ class Task:
         "date_created": self.date_created,
         "deadline": self.deadline
       }
-with open("task.json", "r") as f:
-   data=json.load(f)
+  @classmethod
+  def from_dict(cls, data):
+      task = cls(data["name"], data["priority"], data["deadline"])
+      task.id = data["id"]
+      task.completed = data["completed"]
+      task.date_created = data["date_created"]
+      return task
 
 tasks={1: Task("Finish project", 1, "2026-12-31 18:00"),2: Task("Buy groceries", 2, "2026-09-15 12:00"),3: Task("Call mom", 3, "2026-09-01 20:00")}
 
